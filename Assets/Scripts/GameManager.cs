59c0f5d4948ef4ae22cecae21e15fc918ccfe61c
Sebastian <sebastiamerelor@gmail.com>
2022-03-26 00:38:26 +0100

    scripts

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    [SerializeField]
+    private int currentYear = 0, endYear = 20;
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
