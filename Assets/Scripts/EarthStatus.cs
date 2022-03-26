c4ac6c40252313d2c494c2afa5b60824abbdcd31
YujeK <40034606+YujeK@users.noreply.github.com>
2022-03-26 23:54:05 +0100

    =earth

---
+++ Assets/Scripts/EarthStatus.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EarthStatus : MonoBehaviour
+{
+
+
+    public GameObject[] pollution;
+
+    public GameObject[] ecology;
+
+    public GameObject[] technology;
+
+ //   public void EarthStatus()
+}
