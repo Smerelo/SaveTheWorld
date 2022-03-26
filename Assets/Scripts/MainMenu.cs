d2067baa46a447aa2e08a8c1b5cfd699283a257f
vincentbachelet-collab <vcbachelet@gmail.com>
2022-03-26 18:14:14 +0100

    + first screen

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -2,11 +2,19 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using TMPro;
 
 public class MainMenu : MonoBehaviour
 {
-   public void playGame()
-   {
-        SceneManager.LoadScene(1); // 1 = first scene
-   }
+    public TextMeshProUGUI displayText;
+
+    public void DisplayText()
+    {
+
+        displayText.gameObject.SetActive(true);
+        displayText.text = "What ever you want to display";
+        this.gameObject.SetActive(false);
+
+    }
 }
