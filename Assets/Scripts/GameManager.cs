c9aec847338addd11e84f35efa93aa52e0d1b94b
Sebastian <sebastiamerelor@gmail.com>
2022-03-26 01:44:57 +0100

    Main scene

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,11 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
+using System;
 
 public class GameManager : MonoBehaviour
 {
-    [SerializeField]
-    private int currentYear = 0, endYear = 20;
+    [SerializeField] private int currentYear = 0, endYear = 20;
+    [SerializeField] private TextMeshPro yearText;
+    private int ecology = 50, happiness = 50, science = 50, economy = 50;
+    private bool GameEnded { get; set; }
+    
+
     void Start()
     {
         
@@ -13,6 +19,25 @@ public class GameManager : MonoBehaviour
 
     void Update()
     {
-        
+        if (currentYear == endYear)
+        {
+            EndGame();
+        }
+    }
+
+    private void EndGame()
+    {
+        Debug.Log("Game end");
+        GameEnded = true;
+    }
+
+    public void MakeChoice()
+    {
+        if (!GameEnded)
+        {
+            currentYear++;
+            yearText.text = currentYear.ToString();
+        }
+
     }
 }
