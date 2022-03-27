ad0ef00fac013bb3be68dfb3ce22393223ba1bc5
Sebastian <sebastiamerelor@gmail.com>
2022-03-27 05:24:21 +0200

    mergeee

--- Assets/Scripts/CardButton.cs
+++ Assets/Scripts/CardButton.cs
@@ -8,12 +8,13 @@ public class CardButton : MonoBehaviour
 {
     public TextMeshProUGUI title, description;
     public Sprite sprite;
+
     [HideInInspector]public Card cardData;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+       
     }
 
     // Update is called once per frame
